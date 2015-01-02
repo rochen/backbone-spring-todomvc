fa802801c70951a8bf4279e37cf4fabdc667c9ce
robbie <robbie@mbp13.com>
2015-01-02 22:06:47 +0800

    import backend todomvc sample to get todos from spring

--- src/main/java/harbour/spring/web/TodoController.java
+++ src/main/java/harbour/spring/web/TodoController.java
@@ -8,11 +8,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("/todos")
 public class TodoController {
 	final static Logger logger = LoggerFactory.getLogger(TodoController.class);
@@ -20,8 +21,14 @@ public class TodoController {
 	@Autowired
 	private TodoService todoService;
 
+//	@RequestMapping(method = RequestMethod.GET)
+//	public String index() {
+//		logger.info("index()");
+//		return "todo/index";
+//	}
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Todo> list() {
+	public @ResponseBody List<Todo> list() {
 		logger.info("Listing todos");
 		return todoService.getAllTodo();
 	}
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,12 @@
-(function( window ) {
+/*global $ */
+/*jshint unused:false */
+var app = app || {};
+var ENTER_KEY = 13;
+var ESC_KEY = 27;
+
+$(function () {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
-
-})( window );
+	// kick things off by creating the `App`
+	new app.AppView();
+});
