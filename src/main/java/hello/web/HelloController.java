a59b92f3ab6669d0e003deefbfd70415732c1045
robbie <robbie@mbp13.com>
2015-01-04 15:00:52 +0800

    remove unused todo component

--- src/main/java/hello/web/HelloController.java
+++ src/main/java/hello/web/HelloController.java
@@ -1,12 +1,9 @@
 package hello.web;
 
-import harbour.spring.dao.TodoDao;
-
 import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     final static Logger logger = LoggerFactory.getLogger(HelloController.class);
-
-    @Autowired
-    private TodoDao todoDao;
     
     @RequestMapping("/hello")
     public String hello(Model model) {
         Date now = new Date();
-        logger.info("my call hello to get serverTime [{}]", now);
+        logger.info("call hello to get serverTime [{}]", now);
         model.addAttribute("serverTime", now);
         return "hello";
     }
